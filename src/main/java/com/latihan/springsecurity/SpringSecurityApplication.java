f1aa85ad1c93eaaec4b028a02b6edd3cba195a0d
RYANSUTODIWIRYO <ryan.s.sutodiwiryo@gmail.com>
2020-10-21 14:52:26 +0700

    initial commit

---
+++ src/main/java/com/latihan/springsecurity/SpringSecurityApplication.java
@@ -0,0 +1,14 @@
+package com.latihan.springsecurity;
+
+import com.latihan.springsecurity.repository.UserRepository;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories(basePackageClasses = UserRepository.class)
+public class SpringSecurityApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityApplication.class, args);
+	}
+}
